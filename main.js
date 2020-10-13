0ab5a59d964ed51cc731f5ae262a868e38288fd4
theprivatepark <anneypark@gmail.com>
2020-10-12 23:35:38 -0400

    Done.

--- main.js
+++ main.js
@@ -4,7 +4,55 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+//does not appear when the page first loads ---> before DOMCONTENTLOADED
+//think about where you are locating certain objects
+// does one need to create the object the event listener is triggered on?
+//if it already exists, wait until it's created
+//always make sure event listeners are called after the DOM is loaded****************!!!!!
+// read yo error in the console !!!!!!!!!!!!!!!!!!!!!!!!
+// add event listener takes in string and function
+// fetch takes in string and hash
+// after fetch, .then
 
+
+//add class .hidden to error modal. 
+document.querySelector("#modal").classList.add("hidden") 
+
+//DCL is an event listener on the document
+document.addEventListener("DOMContentLoaded", ()=>{
+  likeButtonListener()
+})
+
+
+function likeButtonListener() {
+  let allButtons = document.querySelectorAll(".like-glyph")
+  allButtons.forEach(oneButton => oneButton.addEventListener("click", () => {
+    //if heart is empty, do mimicservercall
+    //if heart is full, change to empty heart
+    if (oneButton.innerText == EMPTY_HEART) {
+      mimicServerCall()
+       .then(response => {
+         oneButton.innerText = FULL_HEART
+         oneButton.classList.add("activated-heart")
+       })
+       .catch(response => {
+         document.querySelector("#modal").classList.remove("hidden")
+         document.querySelector("#modal").innerText = response
+         setTimeout(() => {document.querySelector("#modal").classList.add("hidden")}, 5000);
+       })
+    }
+    else {
+      oneButton.innerText = EMPTY_HEART
+      oneButton.classList.remove("activated-heart")
+    }
+      
+
+  }))
+}
+
+// params => console.log(params)
+// () => {}
+// function name() {}
 
 
 //------------------------------------------------------------------------------
